8e4bd5ab1cb069f66fd3b32cb3907ac859ffc0ac
Timer24 <hailtimer666@gmail.com>
2026-01-20 21:10:49 +0400

    feature(86ew8yuka): add default styles to app

--- apps/tia-frontend/src/app/app.routes.ts
+++ apps/tia-frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Route } from '@angular/router';
 import { AppB } from './app-b';
 
 export const appRoutes: Route[] = [
-    {
-        path: 'route-b',
-        component: AppB
-    }
+  {
+    path: 'library',
+    loadComponent: () => import('./features/admin/components/library/container/library-container').then(m => m.LibraryContainer),
+  }
 ];
