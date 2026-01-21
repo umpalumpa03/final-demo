a203ca5080e008148604387d690befd081f19ec2
SabaGudadze05 <gitsabagudadze@gmail.com>
2026-01-21 16:22:19 +0400

    feature(86ew9uqm2): add library-header and heading for component

--- apps/tia-frontend/src/app/features/admin/components/library/container/library-container.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/container/library-container.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Navigation } from '../shared/navigation/navigation';
+import { LibraryHeader } from "../shared/library-header/library-header";
 
 @Component({
   selector: 'app-library-container',
-  imports: [RouterModule, Navigation],
+  imports: [RouterModule, Navigation, LibraryHeader],
   templateUrl: './library-container.html',
   styleUrl: './library-container.scss',
 })
---
+++ apps/tia-frontend/src/app/features/admin/components/library/shared/library-header/library-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-header',
+  imports: [],
+  templateUrl: './library-header.html',
+  styleUrl: './library-header.scss',
+})
+export class LibraryHeader {}
