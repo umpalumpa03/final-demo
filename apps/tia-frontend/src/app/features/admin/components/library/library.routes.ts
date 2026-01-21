fb96d1f71cbcec893e80c7354b8e0db5f7908a3e
moonlight <gvantsatediashvili@gmail.com>
2026-01-21 11:29:41 +0400

    fix(86ew87510): Update routes in Navigation

--- apps/tia-frontend/src/app/features/admin/components/library/library.routes.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/library.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const libraryRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./container/library-container').then(m => m.LibraryContainer),
+    loadComponent: () =>
+      import('./container/library-container').then((m) => m.LibraryContainer),
     children: [
       {
-        path: 'components/colorpalettes',
-        loadComponent: () => import('./components/colorpalettes/colorpalettes').then(m => m.Colorpalettes),
+        path: 'colorpalettes',
+        loadComponent: () =>
+          import('./components/colorpalettes/colorpalettes').then(
+            (m) => m.Colorpalettes,
+          ),
       },
     ],
   },
