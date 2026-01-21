8834151e58a2fa64f83942e5f238f687bf841935
mikheili-silagava-tbc-it-academy <mikheili.silagava.1@btu.edu.ge>
2026-01-21 18:42:04 +0400

    feature(86ew9d0hu): alert types with icons - alert test & pipe test

--- apps/tia-frontend/src/app/shared/lib/alerts/alert-types-with-icons/alert-types-with-icons.spec.ts
+++ apps/tia-frontend/src/app/shared/lib/alerts/alert-types-with-icons/alert-types-with-icons.spec.ts
@@ -18,4 +18,29 @@ describe('AlertTypesWithIcons', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('effectiveImgName Computed Property', () => {
+    it('should convert effectiveImgName if alertType is success', () => {
+      fixture.componentRef.setInput('alertType', 'success');
+      fixture.detectChanges();
+      expect(component.effectiveImgName()).toBe('success');
+    });
+
+    it('should convert effectiveImgName if alertType is warning', () => {
+      fixture.componentRef.setInput('alertType', 'warning');
+      fixture.detectChanges();
+      expect(component.effectiveImgName()).toBe('warning');
+    });
+
+    it('should convert effectiveImgName if alertType is error or information', () => {
+      fixture.componentRef.setInput('alertType', 'error');
+      fixture.detectChanges();
+      expect(component.effectiveImgName()).toBe('default');
+    });
+    it('should convert effectiveImgName if alertType is information', () => {
+      fixture.componentRef.setInput('alertType', 'information');
+      fixture.detectChanges();
+      expect(component.effectiveImgName()).toBe('default');
+    });
+  });
 });
