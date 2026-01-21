c2e576c7feb1b24c995071150b1695bb529b73df
moonlight <gvantsatediashvili@gmail.com>
2026-01-22 00:59:35 +0400

    feature(86ew8ywpa): Modify Ocean Blue Theme Template, add styles and logic

--- apps/tia-frontend/src/app/shared/lib/palettes/oceanblue-theme/oceanblue-theme.ts
+++ apps/tia-frontend/src/app/shared/lib/palettes/oceanblue-theme/oceanblue-theme.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Palette } from '../model/palette.model';
+import { OCEANBLUE_PALETTE_DATA } from '../../../../features/admin/components/library/components/colorpalettes/config/palette-data.config';
 
 @Component({
   selector: 'app-oceanblue-theme',
   imports: [],
   templateUrl: './oceanblue-theme.html',
   styleUrl: './oceanblue-theme.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OceanblueTheme {}
+export class OceanblueTheme {
+  public readonly swatches: Palette[] = OCEANBLUE_PALETTE_DATA;
+}
