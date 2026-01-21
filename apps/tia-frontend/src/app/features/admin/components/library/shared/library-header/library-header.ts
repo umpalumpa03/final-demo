040fe7f00591f8a1b723dc1793eb3214d9c03a69
SabaGudadze05 <gitsabagudadze@gmail.com>
2026-01-21 18:28:18 +0400

    feature(86ew9wek8):add change detection strategy to the components

--- apps/tia-frontend/src/app/features/admin/components/library/shared/library-header/library-header.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/shared/library-header/library-header.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { ColorSwitch } from "apps/tia-frontend/src/app/shared/lib/color-switching-buttons/color-switch/color-switch";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ColorSwitch } from 'apps/tia-frontend/src/app/shared/lib/color-switching-buttons/color-switch/color-switch';
 
 @Component({
   selector: 'app-library-header',
   imports: [ColorSwitch],
   templateUrl: './library-header.html',
   styleUrl: './library-header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LibraryHeader {}
--- apps/tia-frontend/src/app/shared/lib/color-switching-buttons/color-switch/color-switch.ts
+++ apps/tia-frontend/src/app/shared/lib/color-switching-buttons/color-switch/color-switch.ts
@@ -1,10 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-color-switch',
   imports: [],
   templateUrl: './color-switch.html',
   styleUrl: './color-switch.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ColorSwitch {
   public isActive = input<boolean>(false);
