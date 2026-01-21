f726be3edca3250484f3d3c3bbc9f81f77bca97e
burdu <23202223@ibsu.edu.ge>
2026-01-21 15:29:26 +0400

    feat(86ew9qhb8): implement structure for overlays library page

--- apps/tia-frontend/src/app/features/admin/components/library/components/overlay/overlay.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/overlay/overlay.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UiModal } from 'apps/tia-frontend/src/app/shared/lib/layout/ui-modal';
 
 @Component({
   selector: 'app-overlay',
-  imports: [],
+  standalone: true,
+  imports: [UiModal],
   templateUrl: './overlay.html',
-  styleUrl: './overlay.scss',
+  styleUrls: ['./overlay.scss'],
 })
-export class Overlay {}
+export class Overlay {
+  public isDialogOpen = signal<boolean>(false);
+  public isFormDialogOpen = signal<boolean>(false);
+  public isLargeDialogOpen = signal<boolean>(false);
+
+  public isDeleteAlertOpen = signal<boolean>(false);
+  public isConfirmAlertOpen = signal<boolean>(false);
+
+  public isRightSheetOpen = signal<boolean>(false);
+  public isLeftSheetOpen = signal<boolean>(false);
+  public isTopSheetOpen = signal<boolean>(false);
+  public isBottomSheetOpen = signal<boolean>(false);
+
+  public isDrawerOpen = signal<boolean>(false);
+
+  public onContextMenu(event: MouseEvent): void {
+    event.preventDefault();
+  }
+}
