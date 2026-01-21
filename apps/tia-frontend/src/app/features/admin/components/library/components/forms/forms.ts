66068f74e140579a41821a273530056717c65136
bsherazadishvili <bsherazadishvili@tbcbank.ge>
2026-01-21 19:29:35 +0400

    feature(86ew873y5): add library forms component

---
+++ apps/tia-frontend/src/app/features/admin/components/library/components/forms/forms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.html',
+  styleUrl: './forms.scss',
+})
+export class Forms {}
