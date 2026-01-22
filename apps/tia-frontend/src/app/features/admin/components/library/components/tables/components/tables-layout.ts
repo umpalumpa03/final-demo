47d8155878ec8d425fd176c099ad46277c3c895a
lasha-arveladze-tbc-it-academy <larveladze.tech@gmail.com>
2026-01-22 14:56:58 +0400

    feature(86ewa7vn9): use reusable showcase card

--- apps/tia-frontend/src/app/features/admin/components/library/components/tables/components/tables-layout.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/tables/components/tables-layout.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LibraryTitle } from '../../../shared/library-title/library-title';
 import { Tables } from '@tia/shared/lib/data-display/components/tables';
 import { TableConfig } from '@tia/shared/lib/data-display/models/table.model';
+import { ShowcaseCard } from '../../../shared/showcase-card/showcase-card';
 
 @Component({
   selector: 'app-tables-layout',
-  imports: [LibraryTitle, Tables],
+  imports: [LibraryTitle, Tables, ShowcaseCard],
   templateUrl: './tables-layout.html',
   styleUrl: './tables-layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
