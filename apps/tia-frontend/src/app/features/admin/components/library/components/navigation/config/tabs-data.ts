b86dc2113579091efaec920e7737766995f84969
natia-saghliani-tbc-it-academy <natia.saghliani@tbcacademy.edu.ge>
2026-01-22 22:52:44 +0400

    feature(86ew9p7e1): create reusable breadcrumb component

--- apps/tia-frontend/src/app/features/admin/components/library/components/navigation/config/tabs-data.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/navigation/config/tabs-data.ts
@@ -1,3 +1,4 @@
+import { Breadcrumb } from "@tia/shared/lib/navigation/models/breadcrumbs.model";
 import { TabItem } from "apps/tia-frontend/src/app/shared/lib/navigation/models/tab.model";
 
 export const TABS: TabItem[] = [
@@ -5,10 +6,31 @@ export const TABS: TabItem[] = [
     { label: 'Analytics', route: 'test' },
     { label: 'Reports', route: 'reports' },
     { label: 'Settings', route: '' }
-];
+] as const;
 
 export const TABS2: TabItem[] = [
-    { label: 'Account', icon: 'svg/notification-icons/account.svg', route: '/admin/library/navigation' },
-    { label: 'Notifications', icon: 'svg/notification-icons/notifications.svg', route: 'test' },
-    { label: 'Preferences', icon: 'svg/notification-icons/setting.svg', route: '' },
-];
+    { label: 'Account', icon: 'images/svg/notification-icons/account.svg', route: '/admin/library/navigation' },
+    { label: 'Notifications', icon: 'images/svg/notification-icons/notifications.svg', route: 'test' },
+    { label: 'Preferences', icon: 'images/svg/notification-icons/setting.svg', route: '' },
+] as const;
+
+export const BREADCRUMBS: Breadcrumb[] = [
+    { label: 'Home', route: '/admin/library/navigation' },
+    { label: 'Products', route: 'test' },
+    { label: 'Current Page', route: '' },
+] as const;
+
+export const BREADCRUMBS2: Breadcrumb[] = [
+    { label: 'Account', icon: 'images/svg/notification-icons/home.svg', route: '/admin/library/navigation' },
+    { label: 'Notifications', route: 'test' },
+    { label: 'Preferences', route: '' },
+] as const;
+
+
+export const BREADCRUMBS3: Breadcrumb[] = [
+    { label: 'Home', route: '/admin/library/navigation' },
+    { label: 'Dashboard', route: 'test' },
+    { label: 'Settings', route: '' },
+    { label: 'Security', route: '' },
+    { label: 'Two-Factor Authentication', route: '' },
+] as const;
