a4b7a5effb999eff7ddec2c60a4bda3253e6afb3
moonlight <gvantsatediashvili@gmail.com>
2026-01-23 00:05:24 +0400

    bugfix(86ew8ywpa): modify navigation component and it's styles and routing, add routing in library component

--- apps/tia-frontend/src/app/features/admin/components/library/shared/navigation/navigation.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/shared/navigation/navigation.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
-export class Navigation {
-}
+export class Navigation {}
