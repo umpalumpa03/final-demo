db98bd4d902cdfd686bf71cbe57966e4992ff56a
mikheili-silagava-tbc-it-academy <mikheili.silagava.1@btu.edu.ge>
2026-01-22 12:35:49 +0400

    fix(86ew9d0gz): BEM convention, styles, nameing update, dynamic class update

--- apps/tia-frontend/src/app/shared/lib/alerts/components/basic-alerts/basic-alerts.ts
+++ apps/tia-frontend/src/app/shared/lib/alerts/components/basic-alerts/basic-alerts.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { baseAlertType } from '../../shared/models/alert.models';
 
 @Component({
@@ -6,22 +11,26 @@ import { baseAlertType } from '../../shared/models/alert.models';
   imports: [],
   templateUrl: './basic-alerts.html',
   styleUrl: './basic-alerts.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BasicAlerts {
   public alertType = input<baseAlertType>('default');
   public alertTitle = input<string>('Default Alert');
-  public alertMessage = input<string>('This is a default alert with important information.');
+  public alertMessage = input<string>(
+    'This is a default alert with important information.',
+  );
 
- public effectiveTitle = computed(() => {
-    if(this.alertType() === 'error' && this.alertTitle() === 'Default Alert') {
+  public alertClass = computed(() => `basic-alert--${this.alertType()}`);
+
+  public effectiveTitle = computed(() => {
+    if (this.alertType() === 'error' && this.alertTitle() === 'Default Alert') {
       return 'Error Alert';
     }
     return this.alertTitle();
   });
 
-  public effectiveMessage = computed(() => { 
-    if(this.alertType() === 'error' && this.alertMessage() === 'This is a default alert with important information.') {
+  public effectiveMessage = computed(() => {
+    if (this.alertType() === 'error' && this.alertMessage() === 'This is a default alert with important information.') {
       return 'This is an error alert with important information.';
     }
     return this.alertMessage();
