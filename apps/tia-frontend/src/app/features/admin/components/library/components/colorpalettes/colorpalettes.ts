bafda1442c07419f054636223e212560bc3717d9
moonlight <gvantsatediashvili@gmail.com>
2026-01-22 21:17:54 +0400

    feature(86ew8ywpa): modify color palettes component and add royal blue theme

--- apps/tia-frontend/src/app/features/admin/components/library/components/colorpalettes/colorpalettes.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/colorpalettes/colorpalettes.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { LibraryTitle } from '../../shared/library-title/library-title';
 import { OceanblueTheme } from '../../../../../../shared/lib/palettes/oceanblue-theme/oceanblue-theme';
+import { RoyalblueTheme } from '../../../../../../shared/lib/palettes/royalblue-theme/royalblue-theme';
+import { ShowcaseCard } from '../../shared/showcase-card/showcase-card';
 
 @Component({
   selector: 'app-colorpalettes',
-  imports: [LibraryTitle, OceanblueTheme],
+  imports: [LibraryTitle, OceanblueTheme, RoyalblueTheme, ShowcaseCard],
   templateUrl: './colorpalettes.html',
   styleUrl: './colorpalettes.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
