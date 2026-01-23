a1a33d6490ea89a94d54bed54ab46df875ab85a9
David Developer <khoshtaria.davit22@gtu.ge>
2026-01-23 17:44:15 +0400

    feature(86ewav1bt):Wild Card  for application refactor

--- apps/tia-frontend/src/app/features/wild-card/wild-card.ts
+++ apps/tia-frontend/src/app/features/wild-card/wild-card.ts
@@ -1,18 +1,13 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ButtonComponent } from '../../shared/lib/primitives/button/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-wild-card',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,RouterLink],
   templateUrl: './wild-card.html',
   styleUrls: ['./wild-card.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WildCardComponent {
-  private readonly router = inject(Router);
-
-  public navigateToLibrary(): void {
-    this.router.navigate(['/admin/library']); 
-  }
 }
