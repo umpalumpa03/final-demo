9e906743ac048c18cba9544bce092eba04764deb
mariamsvanidze0 <mariamsvanidze00@gmail.com>
2026-01-23 16:26:09 +0400

    feature(86ewax72x) fix layout of all library pages

--- apps/tia-frontend/src/app/features/admin/components/library/components/input/input.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/input/input.ts
@@ -5,10 +5,11 @@ import {
   BASIC_INPUT_DEMOS,
   SPECIAL_INPUT_DEMOS,
 } from './config/input-demos.config';
+import { LibraryTitle } from "../../shared/library-title/library-title";
 
 @Component({
   selector: 'app-input',
-  imports: [CommonModule, TextInput],
+  imports: [CommonModule, TextInput, LibraryTitle],
   templateUrl: './input.html',
   styleUrl: './input.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,4 +17,7 @@ import {
 export class Input {
   protected readonly basicInputs = signal(BASIC_INPUT_DEMOS);
   protected readonly specialInputTypes = signal(SPECIAL_INPUT_DEMOS);
+  public readonly title = 'Input Components';
+  public readonly subtitle =
+    'Form input components with various types and states';
 }
