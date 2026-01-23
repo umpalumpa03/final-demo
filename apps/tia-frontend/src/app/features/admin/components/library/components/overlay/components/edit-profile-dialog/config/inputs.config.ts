923f00607031ec5f8cd77b6fa2b308329e9c1e5a
burdu <23202223@ibsu.edu.ge>
2026-01-23 19:14:40 +0400

    feature(86ewaxp05): refactor edit profile dialog to use custom library components

---
+++ apps/tia-frontend/src/app/features/admin/components/library/components/overlay/components/edit-profile-dialog/config/inputs.config.ts
@@ -0,0 +1,11 @@
+  export const modalNameInput = {
+    label: 'Name',
+    required: true,
+    placeholder: 'John Doe',
+  } as const;
+
+    export const modalEmailInput = {
+    label: 'Email',
+    required: true,
+    placeholder: 'john@example.com',
+  } as const;
