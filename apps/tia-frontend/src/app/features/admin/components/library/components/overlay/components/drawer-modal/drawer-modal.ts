bde721bfb0dff4fccc8eff2deaeb63ab8c3771ba
burdu <23202223@ibsu.edu.ge>
2026-01-23 13:02:19 +0400

    feat(86ewau9m1): update drawer components to use lib input

--- apps/tia-frontend/src/app/features/admin/components/library/components/overlay/components/drawer-modal/drawer-modal.ts
+++ apps/tia-frontend/src/app/features/admin/components/library/components/overlay/components/drawer-modal/drawer-modal.ts
@@ -1,14 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { UiDrawer } from '../../../../../../../../shared/lib/overlay/ui-drawer/ui-drawer';
+import { TextInput } from '../../../../../../../../shared/lib/forms/input-field/text-input/text-input';
+import { emailConfig, nameConfig } from './config/inputs.config';
+import { LibraryTitle } from '../../../../shared/library-title/library-title';
 
 @Component({
   selector: 'app-drawer-modal',
-  imports: [UiDrawer],
+  imports: [UiDrawer, TextInput, LibraryTitle],
   templateUrl: './drawer-modal.html',
   styleUrl: './drawer-modal.scss',
 })
 export class DrawerModal {
   public isOpen = signal<boolean>(false);
+  public readonly nameConfig = nameConfig;
+  public readonly emailConfig = emailConfig;
+  public readonly drawerTitle: string = 'Drawer Title';
+  public readonly drawerSubtitle: string =
+    'This is a drawer component optimized for mobile.';
 
   public toggle(): void {
     this.isOpen.update((v) => !v);
--- apps/tia-frontend/src/app/shared/lib/overlay/ui-drawer/ui-drawer.spec.ts
+++ apps/tia-frontend/src/app/shared/lib/overlay/ui-drawer/ui-drawer.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UiDrawer } from './ui-drawer';
-import { By } from '@angular/platform-browser';
 
 describe('UiDrawer', () => {
   let component: UiDrawer;
