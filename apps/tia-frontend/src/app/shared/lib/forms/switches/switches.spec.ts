b3c58d0221dbdf4dfaf7581407e594c2fd9d3184
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-24 01:16:04 +0400

    feature(86ewb0na7): optimize htmt and scss, add unit tests

--- apps/tia-frontend/src/app/shared/lib/forms/switches/switches.spec.ts
+++ apps/tia-frontend/src/app/shared/lib/forms/switches/switches.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Switches } from './switches';
+import { By } from '@angular/platform-browser';
 
 describe('Switches', () => {
   let component: Switches;
@@ -12,10 +13,34 @@ describe('Switches', () => {
 
     fixture = TestBed.createComponent(Switches);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update value and emit output on user interaction', () => {
+    let emittedValue: boolean | undefined;
+    component.checkedChange.subscribe((val) => (emittedValue = val));
+
+    const inputEl = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputEl.checked = true;
+    inputEl.dispatchEvent(new Event('change'));
+
+    expect(component['value']()).toBe(true);
+    expect(emittedValue).toBe(true);
+  });
+
+  it('should sync checked input and respect disabled config', () => {
+    fixture.componentRef.setInput('checked', true);
+    fixture.detectChanges();
+    expect(component['value']()).toBe(true);
+    expect(component['isDisabled']()).toBe(false);
+
+    fixture.componentRef.setInput('config', { disabled: true });
+    fixture.detectChanges();
+
+    expect(component['isDisabled']()).toBe(true);
   });
 });
