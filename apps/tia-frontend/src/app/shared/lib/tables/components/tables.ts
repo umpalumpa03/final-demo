b1eab4af6ed94134dfb4485a989cc4ec059ebb6c
lasha-arveladze-tbc-it-academy <larveladze.tech@gmail.com>
2026-01-23 19:24:08 +0400

    feature(86ewa7w2r): Create Row Selection Table

--- apps/tia-frontend/src/app/shared/lib/tables/components/tables.ts
+++ apps/tia-frontend/src/app/shared/lib/tables/components/tables.ts
@@ -1,13 +1,40 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TableConfig } from '../models/table.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  signal,
+} from '@angular/core';
+import { TableConfig, TableRowCell } from '../models/table.model';
+import { Badges } from '../../primitives/badges/badges';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tables',
-  imports: [],
+  imports: [Badges, DatePipe],
   templateUrl: './tables.html',
   styleUrl: './tables.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Tables {
-  public tableConfig = input.required<TableConfig>();
+  public readonly tableConfig = input.required<TableConfig>();
+
+  public readonly isSelectable = computed(
+    () => this.tableConfig().type === 'row-selection',
+  );
+
+  public allSelected = signal<boolean>(false);
+  public readonly selectedItems = signal<TableRowCell[]>([]);
+
+  public toggleSelectAll(): void {
+    this.allSelected.update((val) => !val);
+
+    this.allSelected()
+      ? this.selectedItems.set(this.tableConfig().rows)
+      : this.selectedItems.set([]);
+  }
+
+  public onIndividualSelect(row: TableRowCell): void {
+    this.selectedItems.update((val) => [...val, row]);
+  }
 }
