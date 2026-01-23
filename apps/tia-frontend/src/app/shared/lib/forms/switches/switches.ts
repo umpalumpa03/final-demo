2812ca45368f27ab2fb12af9686b84836a68aec6
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-24 00:53:54 +0400

    feature(86ewb0na7): implement ts logic, implement html and scss transitions

--- apps/tia-frontend/src/app/shared/lib/forms/switches/switches.ts
+++ apps/tia-frontend/src/app/shared/lib/forms/switches/switches.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  input,
+  output,
+} from '@angular/core';
+import { BaseInput } from '../base/base-input';
+import { generateUniqueId } from '../base/utils/input.util';
+import { SWITCH_DEFAULTS } from '../config/switches.config';
+import { SwitchConfig } from '../models/switches.model';
 
 @Component({
-  selector: 'app-switches',
+  selector: 'lib-switches',
   imports: [],
   templateUrl: './switches.html',
   styleUrl: './switches.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Switches {}
+export class Switches extends BaseInput {
+  public override readonly config = input<SwitchConfig>({});
+
+  public readonly checked = input<boolean>(false);
+  public readonly checkedChange = output<boolean>();
+
+  private readonly defaultId = generateUniqueId('lib-switch');
+
+  protected readonly mergedConfig = computed<SwitchConfig>(() => ({
+    ...SWITCH_DEFAULTS,
+    id: this.config().id || this.defaultId,
+    ...this.config(),
+  }));
+
+  protected override readonly isDisabled = computed<boolean>(() => {
+    return (
+      this.internalDisabled() ||
+      this.state() === 'disabled' ||
+      !!this.mergedConfig().disabled
+    );
+  });
+
+  constructor() {
+    super();
+    effect(() => {
+      const isChecked = this.checked();
+      if (!this.ngControl) {
+        this.value.set(isChecked);
+      }
+    });
+  }
+
+  protected override handleInput(event: Event): void {
+    if (this.isDisabled()) return;
+
+    const target = event.target as HTMLInputElement;
+    const newValue = target.checked;
+
+    this.value.set(newValue);
+    this.onChange(newValue);
+    this.valueChange.emit(newValue);
+    this.checkedChange.emit(newValue);
+  }
+
+  protected override parseInputValue(target: HTMLInputElement): boolean {
+    return target.checked;
+  }
+}
