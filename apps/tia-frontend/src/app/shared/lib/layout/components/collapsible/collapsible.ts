0ac8914bb99ef8ee23e1f6680b4a8a0ec84e981a
burdu <23202223@ibsu.edu.ge>
2026-01-24 17:57:42 +0400

    feature(86ew9bdcc): implement collapsible library item logic

--- apps/tia-frontend/src/app/shared/lib/layout/components/collapsible/collapsible.ts
+++ apps/tia-frontend/src/app/shared/lib/layout/components/collapsible/collapsible.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { CollapsibleConfig } from './models/collapsible.model';
 
 @Component({
   selector: 'app-collapsible',
   imports: [],
   templateUrl: './collapsible.html',
   styleUrl: './collapsible.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Collapsible {}
+export class Collapsible implements OnInit {
+  public readonly config = input.required<CollapsibleConfig>();
+
+  public isOpen = signal<boolean>(false);
+
+  public ngOnInit(): void {
+    if (this.config().isOpenDefault) {
+      this.isOpen.set(true);
+    }
+  }
+
+  public toggle(): void {
+    this.isOpen.update((v) => !v);
+  }
+}
