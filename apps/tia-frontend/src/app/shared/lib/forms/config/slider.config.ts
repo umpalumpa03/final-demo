324f3cf3f8a90b416f5c94a13c48cd4f2c648d5e
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-24 11:35:12 +0400

    feature(86ewb3fp8): create slider component, create model and config

---
+++ apps/tia-frontend/src/app/shared/lib/forms/config/slider.config.ts
@@ -0,0 +1,9 @@
+import { SliderConfig } from '../models/slider.model';
+
+export const SLIDER_DEFAULTS: SliderConfig = {
+  min: 0,
+  max: 100,
+  step: 1,
+  showValueLabel: true,
+  valueSuffix: '',
+};
