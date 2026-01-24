155024964bf9539c3c66dcefcf843f699110860f
Gela Pipia <gelaphiphia10@gmail.com>
2026-01-24 18:36:16 +0400

    feature(86ew9bdew): scroll-area library component

---
+++ apps/tia-frontend/src/app/shared/lib/layout/components/scroll-area/container/scroll-area.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+import { TDirection } from './scroll-area.model';
+
+@Component({
+  selector: 'app-scroll-area',
+  imports: [],
+  templateUrl: './scroll-area.html',
+  styleUrl: './scroll-area.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScrollArea {
+  public direction = input<TDirection>('vertical');
+  public height = input<string>('20rem');
+}
