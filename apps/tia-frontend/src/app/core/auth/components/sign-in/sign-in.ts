46e5c168c0580d8e07dff0cb9056f1debd890d73
mikheili-silagava-tbc-it-academy <mikheili.silagava.1@btu.edu.ge>
2026-01-26 04:14:03 +0400

    feature(86ewbfnt7): setup base routing and components

---
+++ apps/tia-frontend/src/app/core/auth/components/sign-in/sign-in.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.scss',
+})
+export class SignIn {}
