37c8e172b12cb39f04cc13a96abb8b4d3a451193
Gela Pipia <gelaphiphia10@gmail.com>
2026-01-27 19:17:55 +0400

    feature(86ew8761d): add translate pipes in sidebar component

--- apps/tia-frontend/src/app/layout/ui/sidebar/sidebar.ts
+++ apps/tia-frontend/src/app/layout/ui/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
