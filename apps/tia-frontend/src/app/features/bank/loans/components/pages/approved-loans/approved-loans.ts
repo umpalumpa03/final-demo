31febb744da2548474f88156600c0aab1f1de6c3
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-27 21:09:31 +0400

    feature(86ewcf3bv): add approved-loans component

---
+++ apps/tia-frontend/src/app/features/bank/loans/components/pages/approved-loans/approved-loans.ts
@@ -0,0 +1,33 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LoansActions } from '../../../store/loans.actions';
+import { selectFilteredLoans } from '../../../store/loans.selectors';
+import { LoanCard } from '../../../shared/ui/loan-card/loan-card';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-approved-loans',
+  imports: [LoanCard, CommonModule],
+  templateUrl: './approved-loans.html',
+  styleUrl: './approved-loans.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ApprovedLoans implements OnInit {
+  private store = inject(Store);
+
+  approvedLoans$ = this.store.select(selectFilteredLoans(2));
+
+  ngOnInit() {
+    this.store.dispatch(LoansActions.loadLoans());
+  }
+
+  // ES AXLA IYOS SHEMDEG PR-ZE DAVAMATEB MODALEBS DA AMOISHLEBA
+  onCardClick(id: string) {
+    console.log('Clicked', id);
+  }
+}
