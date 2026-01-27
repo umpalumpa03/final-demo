4096aaf013958bc92700355ede101af76548bd56
mariamsvanidze0 <mariamsvanidze00@gmail.com>
2026-01-28 00:37:48 +0400

    fix(86ewc14gr): fix tests

--- apps/tia-frontend/src/app/layout/ui/sidebar/sidebar.ts
+++ apps/tia-frontend/src/app/layout/ui/sidebar/sidebar.ts
@@ -1,12 +1,30 @@
+// import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+// import { Router, RouterLink } from '@angular/router';
+// import { SIDEBARDATA } from './config/routes.config';
+// import { NavigationBar } from '@tia/shared/lib/navigation/navigation-bar/navigation-bar';
+// import { TranslatePipe } from '@ngx-translate/core';
+
+// @Component({
+//   selector: 'app-sidebar',
+//   imports: [RouterLink, TranslatePipe, NavigationBar],
+//   templateUrl: './sidebar.html',
+//   styleUrl: './sidebar.scss',
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+// })
+// export class Sidebar {
+//   public readonly items = SIDEBARDATA;
+//   public readonly router = inject(Router);
+// }
+
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { SIDEBARDATA } from './config/routes.config';
 import { NavigationBar } from '@tia/shared/lib/navigation/navigation-bar/navigation-bar';
-import { TranslatePipe } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, TranslatePipe],
+  imports: [NavigationBar],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
