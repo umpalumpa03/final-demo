4382f4e14b8d3a90dc22dbe5603d497bdadaf716
Timer24 <hailtimer666@gmail.com>
2026-01-28 01:03:37 +0400

    feature(86ew8yme0): move bank routes from features into layout

--- apps/tia-frontend/src/app/app.routes.ts
+++ apps/tia-frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './core/auth/auth.routes';
-import { bankRoutes } from './features/bank/bank.routes';
+import { bankRoutes } from './layout/bank.routes';
 import { storybookRoutes } from './features/storybook/storybook.routes';
 
 export const appRoutes: Routes = [
