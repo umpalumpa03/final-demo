aab13ae7e1bca5d29a8b4b0a812e6a2f588a9439
Timer24 <hailtimer666@gmail.com>
2026-01-27 16:42:01 +0400

    fix(86ew9q4fw): remove '/' from apiUrl

--- apps/tia-frontend/src/environments/environment.prod.ts
+++ apps/tia-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://tia.up.railway.app/"
+  apiUrl: "https://tia.up.railway.app"
 }
--- apps/tia-frontend/src/environments/environment.ts
+++ apps/tia-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://tia.up.railway.app/"
+  apiUrl: "https://tia.up.railway.app"
 }
