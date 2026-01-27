bfe43f607c59536958bb80e4289bef28c4b501b9
moonlight <gvantsatediashvili@gmail.com>
2026-01-27 21:24:38 +0400

    feature(86ew875hq): add account service

---
+++ apps/tia-frontend/src/app/features/bank/products/services/accounts.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {
+  Account,
+  CreateAccountRequest,
+  AccountsResponse,
+} from '../models/account.model';
+import { environment } from '../../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountsService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/accounts`;
+
+  public getAccounts(): Observable<AccountsResponse> {
+    return this.http.get<AccountsResponse>(this.apiUrl);
+  }
+
+  public getAccountById(accountId: string): Observable<Account> {
+    return this.http.get<Account>(`${this.apiUrl}/${accountId}`);
+  }
+
+  public createAccount(request: CreateAccountRequest): Observable<Account> {
+    return this.http.post<Account>(this.apiUrl, request);
+  }
+
+  public makeTransfer(accountId: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/${accountId}/transfer`, {});
+  }
+
+  public getCurrencies(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/catalogs/currencies`);
+  }
+}
