ff7ea115ac02441699dd0600b2d11d5798f85209
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-28 20:19:02 +0400

    feature(86ewd8qjv): add svg, implement loan-details modal open in approved component

--- apps/tia-frontend/src/app/features/bank/loans/components/pages/approved-loans/approved-loans.ts
+++ apps/tia-frontend/src/app/features/bank/loans/components/pages/approved-loans/approved-loans.ts
@@ -3,16 +3,20 @@ import {
   Component,
   inject,
   OnInit,
+  signal,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LoansActions } from '../../../store/loans.actions';
 import { selectFilteredLoans } from '../../../store/loans.selectors';
 import { LoanCard } from '../../../shared/ui/loan-card/loan-card';
 import { CommonModule } from '@angular/common';
+import { ILoan } from '../../../shared/models/loan.model';
+import { take } from 'rxjs';
+import { LoanDetails } from '../../../shared/ui/prepayment-wizard/loan-details/loan-details';
 
 @Component({
   selector: 'app-approved-loans',
-  imports: [LoanCard, CommonModule],
+  imports: [LoanCard, CommonModule, LoanDetails],
   templateUrl: './approved-loans.html',
   styleUrl: './approved-loans.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,13 +26,21 @@ export class ApprovedLoans implements OnInit {
 
   protected readonly approvedLoans$ = this.store.select(selectFilteredLoans(2));
 
+  selectedLoan = signal<ILoan | null>(null);
+  isDetailsOpen = signal<boolean>(false);
+
   public ngOnInit(): void {
     this.store.dispatch(LoansActions.loadLoans());
   }
 
-  // ES AXLA IYOS SHEMDEG PR-ZE DAVAMATEB MODALEBS DA AMOISHLEBA
-  public onCardClick(id: string) {
-    // console.log('Clicked', id);
+  onCardClick(id: string) {
+    this.approvedLoans$.pipe(take(1)).subscribe((loans) => {
+      const loan = loans.find((l) => l.id === id);
+      if (loan) {
+        this.selectedLoan.set(loan);
+        this.isDetailsOpen.set(true);
+      }
+    });
   }
 
   public onRenameLoan(event: { id: string; name: string }): void {
