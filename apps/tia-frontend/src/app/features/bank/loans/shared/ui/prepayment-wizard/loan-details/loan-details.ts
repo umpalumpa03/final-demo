d661f4fae6db7a04bcab7f790d830c52de89fe00
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-28 19:36:59 +0400

    feature(86ewd8qjv): loan-details component create

---
+++ apps/tia-frontend/src/app/features/bank/loans/shared/ui/prepayment-wizard/loan-details/loan-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-details',
+  imports: [],
+  templateUrl: './loan-details.html',
+  styleUrl: './loan-details.scss',
+})
+export class LoanDetails {}
