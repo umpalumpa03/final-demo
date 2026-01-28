fd07a90c0e22147a3c82f990c4a2655d4e19601d
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-28 20:19:41 +0400

    feature(86ewd8qjv): implement logic in loan-details component

--- apps/tia-frontend/src/app/features/bank/loans/shared/ui/prepayment-wizard/loan-details/loan-details.ts
+++ apps/tia-frontend/src/app/features/bank/loans/shared/ui/prepayment-wizard/loan-details/loan-details.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { ILoan } from '../../../models/loan.model';
+import { UiModal } from '@tia/shared/lib/overlay/ui-modal/ui-modal';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
+import { CommonModule } from '@angular/common';
+import { CLOSE_VARIANT } from '../../../config/loan-details.config';
 
 @Component({
   selector: 'app-loan-details',
-  imports: [],
+  imports: [UiModal, ButtonComponent, CommonModule],
   templateUrl: './loan-details.html',
   styleUrl: './loan-details.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoanDetails {}
+export class LoanDetails {
+  public readonly buttonVariant = CLOSE_VARIANT;
+
+  loan = input<ILoan | null>(null);
+
+  isOpen = input.required<boolean>();
+  close = output<void>();
+
+  calculatePrepayment = output<ILoan>();
+
+  onCalculate() {
+    const currentLoan = this.loan();
+    if (currentLoan) {
+      this.calculatePrepayment.emit(currentLoan);
+    }
+  }
+}
