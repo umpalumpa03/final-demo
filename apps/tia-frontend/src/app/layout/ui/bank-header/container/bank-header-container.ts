e719b75f7357d1f9a1dc4f5aad1f759172efd4ca
mariamsvanidze0 <mariamsvanidze00@gmail.com>
2026-01-29 18:30:52 +0400

    feature(86ew875dq): add avatar in header

--- apps/tia-frontend/src/app/layout/ui/bank-header/container/bank-header-container.ts
+++ apps/tia-frontend/src/app/layout/ui/bank-header/container/bank-header-container.ts
@@ -6,12 +6,15 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { BankHeader } from '../components/bank-header/bank-header';
 import { HeaderNotifications } from '../components/header-notifications/header-notifications';
 import { Notifications } from '../service/notifications';
 import { Observable, tap } from 'rxjs';
 import { NotificationsData } from '../modals/notification.model';
 import { InboxService } from '@tia/shared/services/messages/inbox.service';
+import { selectCurrentAvatarUrl } from '../../../../store/profile-photo/profile-photo.selectors';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-bank-header-container',
@@ -21,6 +24,7 @@ import { InboxService } from '@tia/shared/services/messages/inbox.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BankHeaderContainer implements OnInit {
+  private store = inject(Store);
   public headerNotificationService = inject(Notifications);
   public inboxService = inject(InboxService);
 
@@ -29,6 +33,7 @@ export class BankHeaderContainer implements OnInit {
   public isModalOpen = signal<boolean>(false);
   public notificationsItems$!: Observable<NotificationsData>;
   public inboxCount = signal<number>(0);
+  public avatarUrl = toSignal(this.store.select(selectCurrentAvatarUrl));
 
   ngOnInit(): void {
     this.headerNotificationService
