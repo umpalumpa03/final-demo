527fec7308ed14e3a2a582b65917e6b0d82a856f
shvangiradze1010 <shvangiradze1010@gmail.com>
2026-01-29 05:24:02 -0800

    feature(86ewbdrrq): create changde password ui

--- apps/tia-frontend/src/app/features/bank/settings/components/security/components/security.component.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/security/components/security.component.ts
@@ -1,16 +1,40 @@
 import {
   ChangeDetectionStrategy,
   Component,
-
 } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BasicCard } from '@tia/shared/lib/cards/basic-card/basic-card';
+import { TextInput } from '@tia/shared/lib/forms/input-field/text-input';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-security',
-  imports: [],
+  imports: [
+    BasicCard,
+    TextInput,
+    ButtonComponent,
+    ReactiveFormsModule,
+    TranslatePipe,
+  ],
   templateUrl: './security.component.html',
   styleUrl: './security.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SecurityComponent {
+  public readonly changePasswordForm: FormGroup;
 
+  constructor(private readonly fb: FormBuilder) {
+    this.changePasswordForm = this.fb.group({
+      currentPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.changePasswordForm.valid) {
+     console.log('Form submitted:', this.changePasswordForm.value);
+    }
+  }
 }
