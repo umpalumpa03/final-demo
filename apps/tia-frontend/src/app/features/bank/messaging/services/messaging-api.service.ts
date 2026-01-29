6b009ce5948f2184df241493ca81b4f3bd6f47e7
natia-saghliani-tbc-it-academy <natia.saghliani@tbcacademy.edu.ge>
2026-01-29 19:41:31 +0400

    fix(86ewdum31): update messaging api service file

--- apps/tia-frontend/src/app/features/bank/messaging/services/messaging-api.service.ts
+++ apps/tia-frontend/src/app/features/bank/messaging/services/messaging-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../../environments/environment.prod';
+import { environment } from '../../../../../environments/environment';
 import { MailsResponse } from '../store/messaging.state';
 @Injectable({
   providedIn: 'root',
