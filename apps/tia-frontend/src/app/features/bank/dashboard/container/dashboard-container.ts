b741b586600560ebfe2a6d2b1c96544543215032
burdu <23202223@ibsu.edu.ge>
2026-01-29 16:08:34 +0400

    feature(86ewb9zbr): improve styles for dashboard cards

--- apps/tia-frontend/src/app/features/bank/dashboard/container/dashboard-container.ts
+++ apps/tia-frontend/src/app/features/bank/dashboard/container/dashboard-container.ts
@@ -1,45 +1,61 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  computed,
+} from '@angular/core';
 import { DragContainer } from '@tia/shared/lib/drag-n-drop/components/drag-container/drag-container';
 import { DraggableCard } from '@tia/shared/lib/drag-n-drop/components/draggable-card/draggable-card';
 import { DraggableItemType } from '@tia/shared/lib/drag-n-drop/model/drag.model';
+import { DragItemDirective } from '@tia/shared/lib/drag-n-drop/directives/drag-item.directive';
 
 @Component({
   selector: 'app-dashboard-container',
-  imports: [DragContainer, DraggableCard],
+  standalone: true,
+  imports: [DragContainer, DraggableCard, DragItemDirective],
   templateUrl: './dashboard-container.html',
   styleUrl: './dashboard-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardContainer {
-onRemove(arg0: string) {
-throw new Error('Method not implemented.');
-}
-onContainerOrderChange($event: string[]) {
-throw new Error('Method not implemented.');
-}
-onItemsChange($event: DraggableItemType[]) {
-throw new Error('Method not implemented.');
-}
-  protected readonly myItems = [
+  protected readonly myItems = signal<DraggableItemType[]>([
     {
       id: '1',
-      title: 'Item 1',
-      subtitle: 'drag to reorder',
+      title: 'Recent Transactions',
+      subtitle: 'Your latest account activity',
       icon: 'images/svg/drag-and-drop/card.svg',
     },
     {
       id: '2',
-      title: 'Item 2',
-      subtitle: 'drag to reorder',
+      title: 'Accounts',
+      subtitle: 'Your account balances and activity',
       icon: 'images/svg/drag-and-drop/exchange.svg',
     },
     {
       id: '3',
-      title: 'Item 3',
-      subtitle: 'drag to reorder',
+      title: 'Exchange Rates',
+      subtitle: 'Live currency exchange rates',
       icon: 'images/svg/drag-and-drop/folder.svg',
-    }
-  ];
+    },
+  ]);
 
-  onEdit(id:string):void{}
+  protected readonly dynamicColspans = computed(() => {
+    return this.myItems().map((_, index) => (index === 0 ? 2 : 1));
+  });
+
+  public onItemsChange(newItems: DraggableItemType[]): void {
+    this.myItems.set(newItems);
+  }
+
+  public onContainerOrderChange(ids: string[]): void {
+    console.log('New Order saved to DB:', ids);
+  }
+
+  onRemove(id: string) {
+    this.myItems.update((items) => items.filter((i) => i.id !== id));
+  }
+
+  onEdit(id: string) {
+    console.log('Editing widget:', id);
+  }
 }
