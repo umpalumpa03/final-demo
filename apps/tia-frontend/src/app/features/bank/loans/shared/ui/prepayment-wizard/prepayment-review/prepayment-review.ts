6d4b97abbe830e1f2d8fd9e752b4bc439934c2dd
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-01-29 15:50:20 +0400

    feature(86ewdrp26): implement logic in prepayment review component

--- apps/tia-frontend/src/app/features/bank/loans/shared/ui/prepayment-wizard/prepayment-review/prepayment-review.ts
+++ apps/tia-frontend/src/app/features/bank/loans/shared/ui/prepayment-wizard/prepayment-review/prepayment-review.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { IPrepaymentCalcResponse } from '../../../models/prepayment.model';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
 
 @Component({
   selector: 'app-prepayment-review',
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './prepayment-review.html',
   styleUrl: './prepayment-review.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PrepaymentReview {}
+export class PrepaymentReview {
+  public readonly calculationResult = input.required<IPrepaymentCalcResponse>();
+
+  public readonly cancel = output<void>();
+  public readonly confirmPay = output<void>();
+
+  public isCurrency(text: string): boolean {
+    const lowerText = text.toLowerCase();
+    return (
+      lowerText.includes('amount') ||
+      lowerText.includes('savings') ||
+      lowerText.includes('monthly payment') ||
+      lowerText.includes('cost') ||
+      lowerText.includes('interest')
+    );
+  }
+}
