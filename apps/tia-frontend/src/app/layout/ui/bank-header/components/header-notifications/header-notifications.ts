6b43580ad66de75015715c456ac541ae5cf9b5b7
lasha-arveladze-tbc-it-academy <larveladze.tech@gmail.com>
2026-01-30 03:18:19 +0400

    feature(86ew875f0): create initial stage of notifications

--- apps/tia-frontend/src/app/layout/ui/bank-header/components/header-notifications/header-notifications.ts
+++ apps/tia-frontend/src/app/layout/ui/bank-header/components/header-notifications/header-notifications.ts
@@ -1,30 +1,56 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   effect,
   ElementRef,
+  inject,
   input,
   signal,
 } from '@angular/core';
-import { Observable } from 'rxjs';
-import { NotificationsData } from '../../modals/notification.model';
-import { AsyncPipe } from '@angular/common';
+
+import { ItemsData } from '../../models/notification.model';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { ScrollArea } from '@tia/shared/lib/layout/components/scroll-area/container/scroll-area';
+import { Checkboxes } from '@tia/shared/lib/forms/checkboxes/checkboxes';
+import { NotificationsStore } from '../../store/notifications.store';
+import { RouteLoader } from '@tia/shared/lib/feedback/route-loader/route-loader';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
 
 @Component({
   selector: 'app-header-notifications',
-  imports: [AsyncPipe],
+  imports: [
+    ScrollArea,
+    DatePipe,
+    Checkboxes,
+    TitleCasePipe,
+    RouteLoader,
+    ButtonComponent,
+  ],
   templateUrl: './header-notifications.html',
   styleUrl: './header-notifications.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderNotifications {
+  readonly store = inject(NotificationsStore);
+
   public isOpen = input<boolean>();
   public messages = input();
   public anchor = input<ElementRef | undefined>();
-  public notificationsItems = input<Observable<NotificationsData>>();
+
+  // Store working
+  public notificationsItems = this.store.items;
+  public isLoading = this.store.isLoading;
+  public hasError = this.store.hasError;
+  public unreadLeft = this.store.unreadNotificationsNumber;
 
   public top = signal(0);
   public left = signal(0);
+
+  public isAllSelected = computed(() => {
+    return this.notificationsItems().length === this.selectedItems().length;
+  });
+  public selectedItems = signal<string[]>([]);
 
   constructor() {
     effect(() => {
@@ -39,4 +65,49 @@ export class HeaderNotifications {
       this.left.set(rect.right - 380);
     });
   }
+
+  public toggleSelectAll(messages: ItemsData[]) {
+    if (this.isAllSelected()) {
+      this.selectedItems.set([]);
+    } else {
+      this.selectedItems.set(messages.map((item) => item.id));
+    }
+  }
+
+  public individualItemSelection(id: string) {
+    this.selectedItems.update((items) => {
+      const exists = items.includes(id);
+
+      if (exists) {
+        return items.filter((itemId) => itemId !== id);
+      }
+
+      return [...items, id];
+    });
+  }
+
+  public isItemSelected(id: string) {
+    return this.selectedItems().includes(id);
+  }
+
+  public onTrashIconClick(id: string) {
+    this.store.deleteNotification(id);
+  }
+
+  public onReadIconClicked(id: string) {
+    this.store.markItemRead(id);
+  }
+
+  public onMarkAllClick() {
+    this.store.markAllAsRead();
+  }
+
+  // public onScrollBottom() {
+  //   if (this.store.pageInfo.hasNext()) {
+  //     this.store.fetchNotifications({
+  //       cursor: this.store.pageInfo.nextCursor(),
+  //       limit: this.store.limitPerPage(),
+  //     });
+  //   }
+  // }
 }
--- apps/tia-frontend/src/app/layout/ui/bank-header/container/bank-header-container.ts
+++ apps/tia-frontend/src/app/layout/ui/bank-header/container/bank-header-container.ts
@@ -12,10 +12,9 @@ import { BankHeader } from '../components/bank-header/bank-header';
 import { HeaderNotifications } from '../components/header-notifications/header-notifications';
 import { Notifications } from '../service/notifications';
 import { Observable, tap } from 'rxjs';
-import { NotificationsData } from '../modals/notification.model';
+import { NotificationsData } from '../models/notification.model';
 import { InboxService } from '@tia/shared/services/messages/inbox.service';
-import { selectCurrentAvatarUrl } from '../../../../store/profile-photo/profile-photo.selectors';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { NotificationsStore } from '../store/notifications.store';
 
 @Component({
   selector: 'app-bank-header-container',
@@ -23,13 +22,15 @@ import { toSignal } from '@angular/core/rxjs-interop';
   templateUrl: './bank-header-container.html',
   styleUrl: './bank-header-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [NotificationsStore],
 })
 export class BankHeaderContainer implements OnInit {
-  private store = inject(Store);
+  readonly store = inject(NotificationsStore);
+
   public headerNotificationService = inject(Notifications);
   public inboxService = inject(InboxService);
 
-  public hasUnread = signal<boolean>(false);
+  public hasUnread = this.store.hasUnread;
   public anchorEl = signal<ElementRef | undefined>(undefined);
   public isModalOpen = signal<boolean>(false);
   public notificationsItems$!: Observable<NotificationsData>;
@@ -37,23 +38,14 @@ export class BankHeaderContainer implements OnInit {
   public avatarUrl = toSignal(this.store.select(selectCurrentAvatarUrl));
 
   ngOnInit(): void {
-    this.headerNotificationService
-      .hasUnreadNotification()
-      .pipe(
-        tap((value) => {
-          this.hasUnread.set(value.hasUnread);
-        }),
-      )
-      .subscribe();
+    this.store.hasUnreadNotifications();
 
       this.inboxService.fetchInboxCount();
   }
 
   public onNotificationClick(el: ElementRef): void {
     this.anchorEl.set(el);
-    this.notificationsItems$ = this.headerNotificationService
-      .getNotifications()
-      .pipe(tap((data) => console.log(data)));
+    this.store.fetchNotifications({ limit: this.store.limitPerPage() });
     this.isModalOpen.update((v) => !v);
   }
 }
