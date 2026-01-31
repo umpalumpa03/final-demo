16db14ac65419a71ce327285e72d213617e866ff
burdu <23202223@ibsu.edu.ge>
2026-01-31 17:32:41 +0400

    fix(86ewemkf1): implement correct styling for widget different sizes

--- apps/tia-frontend/src/app/features/bank/dashboard/components/shared/base-widget.ts
+++ apps/tia-frontend/src/app/features/bank/dashboard/components/shared/base-widget.ts
@@ -5,6 +5,6 @@ export abstract class BaseWidget {
   public readonly isLarge = input(false);
 
   public readonly widgetHeight = computed(() =>
-    this.isLarge() ? '35rem' : '35rem',
+    this.isLarge() ? '37rem' : '34rem',
   );
 }
