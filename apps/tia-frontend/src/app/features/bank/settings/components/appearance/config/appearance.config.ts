d31206be02a1ec6ea24bd0d34f0daec7c6e7947f
Gela Pipia <gelaphiphia10@gmail.com>
2026-01-31 15:33:51 +0400

    feature(86ew875zm): appearance component

--- apps/tia-frontend/src/app/features/bank/settings/components/appearance/config/appearance.config.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/appearance/config/appearance.config.ts
@@ -5,7 +5,7 @@ export interface Theme {
   cssVariable: string;
 }
 
-export const themes: Theme[] = [
+export const themesConfig: Theme[] = [
   {
     title: 'Ocean Blue',
     subtitle: 'Light and refreshing blue theme',
@@ -24,20 +24,4 @@ export const themes: Theme[] = [
     themeKey: 'deepBlue',
     cssVariable: '--color-primary',
   },
-];
-
-/**
- * Gets the computed color value from a CSS variable for a specific theme
- */
-export function getThemeColor(themeKey: string, cssVariable: string): string {
-  // Create a temporary element with the theme applied
-  const tempEl = document.createElement('div');
-  tempEl.setAttribute('data-theme', themeKey);
-  tempEl.style.display = 'none';
-  document.body.appendChild(tempEl);
-
-  const color = getComputedStyle(tempEl).getPropertyValue(cssVariable).trim();
-  document.body.removeChild(tempEl);
-
-  return color;
-}
+] as const;
--- apps/tia-frontend/src/app/features/bank/settings/components/appearance/container/appearance-container.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/appearance/container/appearance-container.ts
@@ -2,42 +2,73 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  DestroyRef,
+  effect,
   inject,
+  OnInit,
   signal,
 } from '@angular/core';
 import { SettingsBody } from '../../../shared/ui/settings-body/settings-body';
-import { themes, getThemeColor } from '../config/appearance.config';
+import { themesConfig } from '../config/appearance.config';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Store } from '@ngrx/store';
 import { selectActiveTheme } from 'apps/tia-frontend/src/app/store/theme/theme.selectors';
 import { ThemeActions } from 'apps/tia-frontend/src/app/store/theme/theme.actions';
+import { AppearanceService } from '../services/appearance.service';
+import { map, tap } from 'rxjs';
+import { TAvailableThemes } from '../models/appearance.model';
+import { ButtonComponent } from "@tia/shared/lib/primitives/button/button";
+import { Skeleton } from "@tia/shared/lib/feedback/skeleton/skeleton";
 
 @Component({
   selector: 'app-appearance-container',
-  imports: [SettingsBody, TranslatePipe],
+  imports: [SettingsBody, TranslatePipe, ButtonComponent, Skeleton],
+  providers: [AppearanceService],
   templateUrl: './appearance-container.html',
   styleUrl: './appearance-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppearanceContainer {
   private store = inject(Store);
+  private appearanceService = inject(AppearanceService);
+  private destroyRef = inject(DestroyRef);
+
   private activeTheme = this.store.selectSignal(selectActiveTheme);
+  public availableThemes = signal<TAvailableThemes | null>(null);
+
+  public isLoading = this.appearanceService.isLoading;
   
-  public readonly themesWithColors = computed(() =>
-    themes.map((theme) => ({
-      ...theme,
-      colorValue: getThemeColor(theme.themeKey, theme.cssVariable),
-    })),
-  );
+  // public readonly themesWithSubtitle = computed(() => {
+  //   return this.availableThemes()!.map((theme, index) => {
+  //     return {
+  //       ...theme,
+  //       subtitle: themesConfig[index].subtitle
+  //     }
+  //   });
+  // })
+
+
+  ngOnInit(): void {
+    const subscription = this.appearanceService.getAvailableThemes().pipe(
+      map((themes) => {
+        return[...themes].map((theme, index) => ({
+          ...theme,
+          subtitle: themesConfig[index].subtitle
+        }));
+      }),
+      tap(themes => {
+        this.availableThemes.set(themes);
+      })
+    ).subscribe();
+    
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
   public isCurrentTheme(theme: string) {
     return this.activeTheme() === theme;
   }
 
   public onClick(selectedColor: string): void {
-    // const el = event.currentTarget as HTMLElement;
-    // if(!el) return;
-    // el.classList.toggle('active');
     this.setActiveColor(selectedColor)
   }
 
@@ -45,4 +76,21 @@ export class AppearanceContainer {
     if (!selectedColor) return;
     this.store.dispatch(ThemeActions.setTheme({ theme: selectedColor }));
   }
+
+  public getThemeColor(themeKey: string, cssVariable: string): string {
+    const tempEl = document.createElement('div');
+    tempEl.setAttribute('data-theme', themeKey);
+    tempEl.style.display = 'none';
+    document.body.appendChild(tempEl);
+
+    const color = getComputedStyle(tempEl).getPropertyValue(cssVariable).trim();
+    document.body.removeChild(tempEl);
+
+    return color;
+  }
+
+  public onSubmit(): void {
+    console.log("submitted...")
+  }
+
 }
