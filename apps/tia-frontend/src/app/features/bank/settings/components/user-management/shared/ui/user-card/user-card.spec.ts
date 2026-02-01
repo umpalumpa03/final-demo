d4aabc23ed087df07ff71f5cf8f1d71bbeda823a
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-02 03:12:31 +0400

    feature(86ewex6jr): optimize spec.ts files

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.spec.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.spec.ts
@@ -1,9 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserCard } from './user-card';
+import { IUser } from '../../models/users.model';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 
 describe('UserCard', () => {
   let component: UserCard;
   let fixture: ComponentFixture<UserCard>;
+
+  const mockUser = {
+    id: '123',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'email@g.com',
+    role: 'CONSUMER',
+    isBlocked: false,
+    createdAt: 'odesghac',
+  } as IUser;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,10 +24,13 @@ describe('UserCard', () => {
 
     fixture = TestBed.createComponent(UserCard);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.componentRef.setInput('user', mockUser);
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have output emitters defined', () => {
+    expect(component.onDetails).toBeDefined();
+    expect(component.onEdit).toBeDefined();
+    expect(component.onDelete).toBeDefined();
   });
 });
