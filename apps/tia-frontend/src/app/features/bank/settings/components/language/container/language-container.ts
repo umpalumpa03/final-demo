4cc537e6556e81ef4f2c2d2c49f4649504ad942c
Gela Pipia <gelaphiphia10@gmail.com>
2026-02-01 17:18:03 +0400

    feature(86ew8761d): improve UI for langauge page

--- apps/tia-frontend/src/app/features/bank/settings/components/language/container/language-container.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/language/container/language-container.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, } from '@angular/core';
 import { LanguageSwitcher } from "../components/language-switcher/language-switcher";
 import { TranslatePipe } from '@ngx-translate/core';
+import { SettingsBody } from "../../../shared/ui/settings-body/settings-body";
 
 
 @Component({
   selector: 'app-language-container',
-  imports: [LanguageSwitcher, TranslatePipe],
+  imports: [LanguageSwitcher, TranslatePipe, SettingsBody],
   templateUrl: './language-container.html',
   styleUrl: './language-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
