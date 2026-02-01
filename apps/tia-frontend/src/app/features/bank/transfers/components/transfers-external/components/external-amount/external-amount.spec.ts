bfc9c64c25d985edd414653926fbe89936366e38
mariamsvanidze0 <mariamsvanidze00@gmail.com>
2026-02-02 02:03:18 +0400

    feature(86ew875np):add transfer services

--- apps/tia-frontend/src/app/features/bank/transfers/components/transfers-external/components/external-amount/external-amount.spec.ts
+++ apps/tia-frontend/src/app/features/bank/transfers/components/transfers-external/components/external-amount/external-amount.spec.ts
@@ -30,6 +30,15 @@ describe('ExternalAmount', () => {
       hasInsufficientBalance: signal(false),
       requiresOtp: signal(false),
       transferSuccess: signal(false),
+
+      setAmount: vi.fn(),
+      setInsufficientBalance: vi.fn(),
+      setLoading: vi.fn(),
+      updateFeeInfo: vi.fn(),
+      setDescription: vi.fn(),
+      setManualRecipientName: vi.fn(),
+      setSelectedRecipientAccount: vi.fn(),
+      setSenderAccount: vi.fn(),
     };
 
     mockExternalService = {
