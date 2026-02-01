809582d391d94dccbd1d30a30c61b8062a030296
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-02 02:29:46 +0400

    feature(86ewex6jr): implement user-card in user management component, make user-card with inputs

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/components/user-management/user-management.component.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/components/user-management/user-management.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TextInput } from '@tia/shared/lib/forms/input-field/text-input';
 import { InputFieldValue } from '@tia/shared/lib/forms/models/input.model';
 import { UserManagementState } from '../../shared/state/user-management.state';
+import { UserManagementStore } from '../../store/user-management.store';
+import { UserCard } from '../../shared/ui/user-card/user-card';
 
 @Component({
   selector: 'app-user-management',
-  imports: [TextInput],
+  imports: [TextInput, UserCard],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.scss',
   providers: [UserManagementState],
@@ -15,4 +17,10 @@ export class UserManagementComponent {
   public readonly userState = inject(UserManagementState);
 
   public onSearch(query: InputFieldValue) {}
+
+  protected readonly store = inject(UserManagementStore);
+
+  ngOnInit() {
+    this.store.loadUsers();
+  }
 }
--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.ts
@@ -1,18 +1,31 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+  signal,
+} from '@angular/core';
 import { BasicCard } from '@tia/shared/lib/cards/basic-card/basic-card';
 import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
+import { IUser } from '../../models/users.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-card',
-  imports: [BasicCard, ButtonComponent],
+  imports: [BasicCard, ButtonComponent, CommonModule],
   templateUrl: './user-card.html',
   styleUrl: './user-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserCard {
-  public isBlocked = signal(false);
+  public readonly user = input.required<IUser>();
 
-  public block() {
-    this.isBlocked.update((v) => !v);
+  public readonly onBlockToggle = output<string>();
+  public readonly onDetails = output<string>();
+  public readonly onEdit = output<string>();
+  public readonly onDelete = output<string>();
+
+  public toggleBlock(): void {
+    this.onBlockToggle.emit(this.user().id);
   }
 }
