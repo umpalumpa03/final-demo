2c49688abbb2b87d459c6e443bacfdcfab31a3e1
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-02 02:11:26 +0400

    feature(86ewex6jr): add user-card logic with dummy text

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/container/user-management-container.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/container/user-management-container.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { UserManagementComponent } from '../components/user-management/user-management.component';
 import { BasicCard } from '@tia/shared/lib/cards/basic-card/basic-card';
 import { UserManagementState } from '../shared/state/user-management.state';
+import { UserCard } from '../shared/ui/user-card/user-card';
 
 @Component({
   selector: 'app-user-management-container',
-  imports: [BasicCard, UserManagementComponent],
+  imports: [BasicCard, UserManagementComponent, UserCard],
   templateUrl: './user-management-container.html',
   styleUrl: './user-management-container.scss',
   providers: [UserManagementState],
--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-card/user-card.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { BasicCard } from '@tia/shared/lib/cards/basic-card/basic-card';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [BasicCard, ButtonComponent],
   templateUrl: './user-card.html',
   styleUrl: './user-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserCard {}
+export class UserCard {
+  public isBlocked = signal(false);
+
+  public block() {
+    this.isBlocked.update((v) => !v);
+  }
+}
