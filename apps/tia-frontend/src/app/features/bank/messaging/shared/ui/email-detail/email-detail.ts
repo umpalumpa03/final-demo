624876a4875fae9597d5dbd0b97801891feac6d3
natia-saghliani-tbc-it-academy <natia.saghliani@tbcacademy.edu.ge>
2026-02-03 01:02:54 +0400

    feature(86ewfgx8r): add message detail view

---
+++ apps/tia-frontend/src/app/features/bank/messaging/shared/ui/email-detail/email-detail.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { EmailDetailData } from '../../../store/messaging.state';
+import { DatePipe } from '@angular/common';
+import { Avatar } from '@tia/shared/lib/data-display/avatars/avatar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouteLoader } from "@tia/shared/lib/feedback/route-loader/route-loader";
+import { MessagingStore } from '../../../store/messaging.store';
+
+@Component({
+  selector: 'app-email-detail',
+  imports: [Avatar, DatePipe, RouteLoader],
+  templateUrl: './email-detail.html',
+  styleUrl: './email-detail.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmailDetail {
+  public readonly data = input<EmailDetailData | undefined>();
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private messagingStore = inject(MessagingStore);
+  public isLoading = this.messagingStore.isLoading;
+
+  public getInitials(email?: string): string {
+    if (!email) return '';
+    return email.substring(0, 2).toUpperCase();
+  }
+
+  public goBack(): void {
+     this.router.navigate(['..'], { relativeTo: this.route });
+  }
+}
