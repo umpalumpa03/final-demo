658a4dbb0e3c4e3e66c343b1d4a80e95d25f00cc
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-02 19:50:24 +0400

    feature(86ewfbdhh): delete confirm modal create, improve userdetails scss with mixins

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/confirm-modal/confirm-modal.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/confirm-modal/confirm-modal.ts
@@ -1,10 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { UiModal } from '@tia/shared/lib/overlay/ui-modal/ui-modal';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
 
 @Component({
   selector: 'app-confirm-modal',
-  imports: [],
+  imports: [UiModal, ButtonComponent],
   templateUrl: './confirm-modal.html',
   styleUrl: './confirm-modal.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfirmModal {}
+export class ConfirmModal {
+  public readonly isOpen = input<boolean>(false);
+  public close = output<void>();
+  public delete = output<void>();
+}
