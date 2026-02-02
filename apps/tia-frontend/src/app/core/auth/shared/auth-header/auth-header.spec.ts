b0fae1f574a5a764c8637be48ab38468e26ce109
bsherazadishvili <bsherazadishvili@tbcbank.ge>
2026-02-02 04:36:26 +0400

    feature(86ewc0t7u): add styling to auth pages

---
+++ apps/tia-frontend/src/app/core/auth/shared/auth-header/auth-header.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthHeader } from './auth-header';
+
+describe('AuthHeader', () => {
+  let component: AuthHeader;
+  let fixture: ComponentFixture<AuthHeader>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AuthHeader],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AuthHeader);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
