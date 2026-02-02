29960713b9c2bd87851368b56c5046986a720cf4
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-02 17:28:44 +0400

    feature(86ewfa0wj): details modal create, html and ts logic implement

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-details-modal/user-details-modal.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/shared/ui/user-details-modal/user-details-modal.ts
@@ -1,10 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { UiModal } from '@tia/shared/lib/overlay/ui-modal/ui-modal';
+import { IUserDetail } from '../../models/users.model';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
 
 @Component({
   selector: 'app-user-details-modal',
-  imports: [],
+  imports: [UiModal, CommonModule, ButtonComponent],
   templateUrl: './user-details-modal.html',
   styleUrl: './user-details-modal.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserDetailsModal {}
+export class UserDetailsModal {
+  public readonly isOpen = input<boolean>(false);
+  public userData = input.required<IUserDetail>();
+  public close = output<void>();
+}
