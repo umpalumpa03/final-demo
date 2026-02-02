74f52bae9f2b5593764c2eb7fce964b896ba1f50
Timer24 <hailtimer666@gmail.com>
2026-02-02 04:11:43 +0400

    feature(86ewezd9e): add infinite load more scroll

--- apps/tia-frontend/src/app/features/bank/dashboard/components/widget-transactions/widget-transactions.ts
+++ apps/tia-frontend/src/app/features/bank/dashboard/components/widget-transactions/widget-transactions.ts
@@ -1,16 +1,17 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  computed,
+  computed, DestroyRef, ElementRef,
   inject,
-  input,
+  input, OnDestroy, ViewChild
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TransactionActions } from 'apps/tia-frontend/src/app/store/transactions/transactions.actions';
 import {
   selectItems,
   selectIsLoading,
-  selectError,
+  selectError, selectNextCursor
 } from 'apps/tia-frontend/src/app/store/transactions/transactions.selector';
 import { AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe } from '@angular/common';
 import { RouteLoader } from '@tia/shared/lib/feedback/route-loader/route-loader';
@@ -20,6 +21,7 @@ import { ScrollArea } from '@tia/shared/lib/layout/components/scroll-area/contai
 import { BaseWidget } from '../shared/base-widget.config';
 import { CurrencySymbolPipe } from 'apps/tia-frontend/src/app/features/bank/dashboard/pipes/currency-symbols.pipe';
 import { TranslateModule } from '@ngx-translate/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-widget-transactions',
@@ -30,18 +32,25 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class WidgetTransactions extends BaseWidget {
   private readonly store = inject(Store);
+  private readonly destroyRef = inject(DestroyRef);
+
+  @ViewChild('loadTrigger', { static: false }) loadTrigger?: ElementRef;
 
   public isLoading$ = this.store.select(selectIsLoading);
   public error$ = this.store.select(selectError);
-
   public transactions$ = this.store.select(selectItems);
+  public nextCursor$ = this.store.select(selectNextCursor);
+
+  public onScrollBottom(): void {
+    this.loadMore();
+  }
 
   public retryLoad(): void {
-    this.store.dispatch(
-      TransactionActions.updateFilters({
-        filters: { pageLimit: 10 },
-      }),
-    );
+    this.store.dispatch(TransactionActions.enter());
+  }
+
+  public loadMore(): void {
+    this.store.dispatch(TransactionActions.loadMore());
   }
 
   public isEmpty$ = this.transactions$.pipe(
