a496ccce5e57db4c4f023060ea749a48243611cb
burdu <23202223@ibsu.edu.ge>
2026-02-06 20:32:10 +0400

    feature(86ewh9gt6): implement starting banner carousel component

--- apps/tia-frontend/src/app/features/bank/dashboard/components/shared/ui/banner-carousel/banner-carousel.ts
+++ apps/tia-frontend/src/app/features/bank/dashboard/components/shared/ui/banner-carousel/banner-carousel.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { BannerSlide } from '../../models/banner.model';
 
 @Component({
   selector: 'app-banner-carousel',
   imports: [],
   templateUrl: './banner-carousel.html',
   styleUrl: './banner-carousel.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BannerCarousel {}
+export class BannerCarousel {
+  private readonly router = inject(Router);
+  public readonly slides = input.required<BannerSlide[]>();
+  protected readonly currentIndex = signal(0);
+}
