853394d04f72e0a44c932dbd8bbc2e6a36493f29
moonlight <gvantsatediashvili@gmail.com>
2026-02-09 00:17:41 +0400

    feature(86ewhqt71): add translations to library component color palettes

--- apps/tia-frontend/src/app/shared/lib/palettes/palettes.ts
+++ apps/tia-frontend/src/app/shared/lib/palettes/palettes.ts
@@ -2,10 +2,12 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   input,
 } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Palette } from './model/palette.model';
-import { palettesData } from '../../../features/storybook/components/colorpalettes/config/palette-data.config';
+import { getPalettesData } from '../../../features/storybook/components/colorpalettes/config/palette-data.config';
 @Component({
   selector: 'app-palettes',
   imports: [],
@@ -14,13 +16,16 @@ import { palettesData } from '../../../features/storybook/components/colorpalett
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Palettes {
+  private readonly translate = inject(TranslateService);
   public theme = input<string>();
 
-  public swatches = computed<Palette[]>(
-    () =>
+  public swatches = computed<Palette[]>(() => {
+    const palettesData = getPalettesData(this.translate);
+    return (
       palettesData[this.theme() as keyof typeof palettesData] ??
-      palettesData.oceanblue,
-  );
+      palettesData.oceanblue
+    );
+  });
 
   public showInsideLabels = computed<boolean[]>(() =>
     this.swatches().map(
