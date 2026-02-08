55b0630aa1aaf9f53f0e48bcdae4b9e4572ec8bf
Gela Pipia <gelaphiphia10@gmail.com>
2026-02-08 12:13:55 +0400

    feature(86ewh8ez0): no-connection component

---
+++ apps/tia-frontend/src/app/features/no-connection/container/no-connection.ts
@@ -0,0 +1,43 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+  OnInit,
+} from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UiModal } from '@tia/shared/lib/overlay/ui-modal/ui-modal';
+import { NoConnectionService } from '../service/no-connection.service';
+
+@Component({
+  selector: 'app-no-connection',
+  imports: [UiModal, TranslatePipe],
+  templateUrl: './no-connection.html',
+  styleUrl: './no-connection.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NoConnection implements OnInit {
+  private noConnectionService = inject(NoConnectionService);
+
+  public isModalOpen = signal(false);
+  public isReconnected = signal(false);
+
+  ngOnInit(): void {
+    if (!this.noConnectionService.isOnline()) {
+      this.isModalOpen.set(true);
+    }
+
+    this.noConnectionService.onOnline = () => {
+      this.isReconnected.set(true);
+      setTimeout(() => {
+        this.isModalOpen.set(false);
+        this.isReconnected.set(false);
+      }, 2000);
+    };
+
+    this.noConnectionService.onOffline = () => {
+      this.isReconnected.set(false);
+      this.isModalOpen.set(true);
+    };
+  }
+}
