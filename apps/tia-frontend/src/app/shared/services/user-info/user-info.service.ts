77ed7e363a5db9d9e253782385db6f2a97f20c7d
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-08 16:33:39 +0400

    feature(86ewhnuv9): create updateOnboardingStatus service

--- apps/tia-frontend/src/app/shared/services/user-info/user-info.service.ts
+++ apps/tia-frontend/src/app/shared/services/user-info/user-info.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IUserInfo, User } from '@tia/shared/models/user-info/user-info.models';
 import { environment } from '../../../../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserInfoService {
@@ -18,5 +18,28 @@ export class UserInfoService {
     return this.http.get<User[]>(`${this.baseUrl}/search-by-email`, {
       params,
     });
+  }
+
+  public updateOnboardingStatus(
+    completed: boolean,
+  ): Observable<{ success: boolean }> {
+    const body = { hasCompletedOnboarding: completed };
+
+    return this.http
+      .put<{ success: boolean }>(`${this.baseUrl}/onboarding-status`, body)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage =
+            'An error occurred while updating onboarding status.';
+
+          if (error.status === 401) {
+            errorMessage = 'Unauthorized: Please sign in again.';
+          } else if (error.error && error.error.message) {
+            errorMessage = error.error.message;
+          }
+
+          return throwError(() => errorMessage);
+        }),
+      );
   }
 }
