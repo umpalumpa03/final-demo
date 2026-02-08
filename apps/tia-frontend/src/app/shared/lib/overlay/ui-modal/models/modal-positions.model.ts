3c22dbc0be04bfecb645772ecb0c583f0c4dd263
burdu <23202223@ibsu.edu.ge>
2026-02-08 15:28:24 +0400

    feature(86ewhmbmg): add configurations for modal positions

---
+++ apps/tia-frontend/src/app/shared/lib/overlay/ui-modal/models/modal-positions.model.ts
@@ -0,0 +1,14 @@
+export interface SpotlightConfig {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+export interface ModalPosition {
+  top?: string;
+  left?: string;
+  right?: string;
+  bottom?: string;
+  transform?: string;
+}
