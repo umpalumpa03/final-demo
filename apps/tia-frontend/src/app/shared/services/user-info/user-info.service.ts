26aa98cf661fe8b699dcbd262a6cc5c8af799f85
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-09 00:38:28 +0400

    feature(86ewhpcpf): sync user-info to main branch

--- apps/tia-frontend/src/app/shared/services/user-info/user-info.service.ts
+++ apps/tia-frontend/src/app/shared/services/user-info/user-info.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IUserInfo, User } from '@tia/shared/models/user-info/user-info.models';
 import { environment } from '../../../../environments/environment';
-import { catchError, Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserInfoService {
@@ -25,21 +25,9 @@ export class UserInfoService {
   ): Observable<{ success: boolean }> {
     const body = { hasCompletedOnboarding: completed };
 
-    return this.http
-      .put<{ success: boolean }>(`${this.baseUrl}/onboarding-status`, body)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          let errorMessage =
-            'An error occurred while updating onboarding status.';
-
-          if (error.status === 401) {
-            errorMessage = 'Unauthorized: Please sign in again.';
-          } else if (error.error && error.error.message) {
-            errorMessage = error.error.message;
-          }
-
-          return throwError(() => errorMessage);
-        }),
-      );
+    return this.http.put<{ success: boolean }>(
+      `${this.baseUrl}/onboarding-status`,
+      body,
+    );
   }
 }
