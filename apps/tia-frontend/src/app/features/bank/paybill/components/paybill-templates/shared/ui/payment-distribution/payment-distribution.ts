cbbda354f7eaa2910e489bee38085a19c38a14d4
lasha-arveladze-tbc-it-academy <larveladze.tech@gmail.com>
2026-02-10 03:43:10 +0400

    feature(86ewhx1rm): fix change detection

--- apps/tia-frontend/src/app/features/bank/paybill/components/paybill-templates/shared/ui/payment-distribution/payment-distribution.ts
+++ apps/tia-frontend/src/app/features/bank/paybill/components/paybill-templates/shared/ui/payment-distribution/payment-distribution.ts
@@ -1,18 +1,76 @@
-import { Component, input, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+  output,
+  signal,
+} from '@angular/core';
 import { ButtonComponent } from '@tia/shared/lib/primitives/button/button';
-import { TotalAmount } from '../total-amount-distribute/total-amount';
+import { TextInput } from '@tia/shared/lib/forms/input-field/text-input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, startWith, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CurrencyPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { TemplatesPageActions } from '../../../../../store/paybill.actions';
+import { selectDistributedAmount } from '../../../../../store/paybill.selectors';
 
 @Component({
   selector: 'app-payment-distribution',
-  imports: [ButtonComponent, TotalAmount],
+  imports: [ButtonComponent, TextInput, ReactiveFormsModule, CurrencyPipe],
   templateUrl: './payment-distribution.html',
   styleUrl: './payment-distribution.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaymentDistribution {
+export class PaymentDistribution implements OnInit {
+  private readonly store = inject(Store);
   public distributionMode = signal<'individual' | 'equal'>('individual');
   public selectedItemsLength = input<number>();
 
+  public isDistribution = output<boolean>();
+
   public onDistributionChange(mode: 'individual' | 'equal'): void {
     this.distributionMode.set(mode);
+    mode === 'equal'
+      ? this.isDistribution.emit(true)
+      : this.isDistribution.emit(false);
+
+    this.amountControl.setValue('');
+  }
+
+  private readonly destroyRef = inject(DestroyRef);
+  public amountControl = new FormControl('');
+
+  public distributedAmount = this.store.selectSignal(selectDistributedAmount);
+
+  ngOnInit(): void {
+    this.amountControl.valueChanges
+      .pipe(
+        startWith(''),
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntilDestroyed(this.destroyRef),
+        tap((value) => {
+          const distributedAmount = value
+            ? +value / (this.selectedItemsLength() ?? 1)
+            : 0;
+
+          this.store.dispatch(
+            TemplatesPageActions.setTotalAmount({
+              amount: +value!,
+            }),
+          );
+
+          this.store.dispatch(
+            TemplatesPageActions.setDistributedAmount({
+              amount: distributedAmount,
+            }),
+          );
+        }),
+      )
+      .subscribe();
   }
 }
