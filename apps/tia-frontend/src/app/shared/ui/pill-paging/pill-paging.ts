5747840f827722ce203b92e0c1006a7614f2747e
burdu <23202223@ibsu.edu.ge>
2026-02-10 18:18:15 +0400

    feature(86ewj4j4v): implement pill paging component styling

--- apps/tia-frontend/src/app/shared/ui/pill-paging/pill-paging.ts
+++ apps/tia-frontend/src/app/shared/ui/pill-paging/pill-paging.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-pill-paging',
   imports: [],
   templateUrl: './pill-paging.html',
   styleUrl: './pill-paging.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PillPaging {}
+export class PillPaging {
+  public readonly total = input.required<number>();
+  public readonly currentIndex = input.required<number>();
+  public readonly goTo = output<number>();
+
+  protected readonly pageArray = computed(() => Array(this.total()));
+}
