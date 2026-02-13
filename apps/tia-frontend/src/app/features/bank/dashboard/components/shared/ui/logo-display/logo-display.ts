ac7e9f5bbc45226c4362ee5a3638b514485c06fd
burdu <23202223@ibsu.edu.ge>
2026-02-13 19:11:23 +0400

    feature(86ewkvew0): implement tooltip for customize button

--- apps/tia-frontend/src/app/features/bank/dashboard/components/shared/ui/logo-display/logo-display.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-logo-display',
-  imports: [],
-  templateUrl: './logo-display.html',
-  styleUrl: './logo-display.scss',
-})
-export class LogoDisplay {}
