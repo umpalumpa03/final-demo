48d530c59ea5b623c4ee5f75250e312dad17e13a
Gela Pipia <gelaphiphia10@gmail.com>
2026-02-13 16:08:55 +0400

    fix(86ewejhvz): use tabs reusable component in settings-header

--- apps/tia-frontend/src/app/features/bank/settings/shared/ui/settings-header/settings-header.ts
+++ apps/tia-frontend/src/app/features/bank/settings/shared/ui/settings-header/settings-header.ts
@@ -1,26 +1,36 @@
 import { Component, computed, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ChangeDetectionStrategy } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import {
   SETTINGS_NAV_ADMIN_CONFIG,
   SETTINGS_NAV_CONFIG,
 } from '../../config/settings-header.config';
 import { Store } from '@ngrx/store';
 import { selectUserRole } from 'apps/tia-frontend/src/app/store/user-info/user-info.selectors';
+import { Tabs } from "@tia/shared/lib/navigation/tabs/tabs";
 
 @Component({
   selector: 'app-settings-header',
-  imports: [RouterLink, RouterLinkActive, TranslatePipe],
+  imports: [TranslatePipe, Tabs],
   templateUrl: './settings-header.html',
   styleUrl: './settings-header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsHeader {
   private readonly store = inject(Store);
+  private readonly translate = inject(TranslateService);
 
-  public readonly navItems = SETTINGS_NAV_CONFIG;
-  public readonly adminNavItems = SETTINGS_NAV_ADMIN_CONFIG;
+  public readonly navItems = SETTINGS_NAV_CONFIG.map((item) => ({
+    label: this.translate.instant(item.translateString),
+    route: item.routerLink,
+    icon: item.src,
+  }));
+
+  public readonly adminNavItems = SETTINGS_NAV_ADMIN_CONFIG.map((item) => ({
+    label: this.translate.instant(item.translateString),
+    route: item.routerLink,
+    icon: item.src,
+  }));
 
   private readonly userRole = this.store.selectSignal(selectUserRole);
 
