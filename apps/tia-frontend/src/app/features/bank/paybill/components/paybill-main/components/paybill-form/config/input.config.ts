030656db53c2e7c34e0344215f7956b2823c4582
burdu <23202223@ibsu.edu.ge>
2026-02-15 02:17:54 +0400

    feature(86ewhnbm2): implement drop down logic for dynamic inputs

--- apps/tia-frontend/src/app/features/bank/paybill/components/paybill-main/components/paybill-form/config/input.config.ts
+++ apps/tia-frontend/src/app/features/bank/paybill/components/paybill-main/components/paybill-form/config/input.config.ts
@@ -7,7 +7,7 @@ export const paybillInputConfig = {
   amount: {
     label: 'paybill.main.form.amount_label',
     placeholder: '0.00',
-    min: 0,
+    min: 10,
     max: 9999,
     required: true,
     errorMessage: 'paybill.main.form.amount_range_error',
