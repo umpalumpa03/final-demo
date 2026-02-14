729b0e1608de07c7aaf36c45945afffe8fff381f
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-02-14 20:14:36 +0400

    feature(86ewhr7u6): add alerts to the user management store

--- apps/tia-frontend/src/app/features/bank/settings/components/user-management/store/user-management.state.ts
+++ apps/tia-frontend/src/app/features/bank/settings/components/user-management/store/user-management.state.ts
@@ -19,3 +19,19 @@ export const initialState: UserState = {
   processingIds: [],
   error: null,
 };
+
+export const UserSuccessKeys = {
+  DELETE: 'settings.user-management.alerts.success.delete_success',
+  UPDATE: 'settings.user-management.alerts.success.update_success',
+  BLOCK: 'settings.user-management.alerts.success.block_success',
+  UNBLOCK: 'settings.user-management.alerts.success.unblock_success',
+};
+
+export const UserErrorKeys = {
+  LOAD_USERS: 'settings.user-management.alerts.error.load_error',
+  LOAD_DETAILS: 'settings.user-management.alerts.error.details_error',
+  DELETE: 'settings.user-management.alerts.error.delete_error',
+  UPDATE: 'settings.user-management.alerts.error.update_error',
+  BLOCK: 'settings.user-management.alerts.error.block_error',
+  NETWORK_ERROR: 'settings.user-management.alerts.error.network_error',
+};
