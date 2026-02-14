8162a58a7edb7d3db46f6b269c995ac8d20896fb
bsherazadishvili <bsherazadishvili@tbcbank.ge>
2026-02-14 16:37:01 +0400

    feature(86ewkz7rw): fix translate

--- apps/tia-frontend/src/app/core/i18n/lazy-translate-loader.ts
+++ apps/tia-frontend/src/app/core/i18n/lazy-translate-loader.ts
@@ -1,31 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslationObject } from '@ngx-translate/core';
 import { forkJoin, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { catchError, map } from 'rxjs/operators';
+ 
 const CORE_TRANSLATION_FILES = [
   'common',
   'auth',
   'sidebar',
   'header-notifications',
-  'storybook'
+  'storybook',
 ] as const;
-
+ 
 export class LazyTranslateLoader implements TranslateLoader {
+  private http: HttpClient;
+ 
   constructor(
-    private http: HttpClient,
+    httpBackend: HttpBackend,
     private prefix = '/i18n/',
     private suffix = '.json',
-  ) {}
-
+  ) {
+    this.http = new HttpClient(httpBackend);
+  }
+ 
   getTranslation(lang: string): Observable<TranslationObject> {
     const requests = CORE_TRANSLATION_FILES.map((file) =>
       this.http
         .get<TranslationObject>(`${this.prefix}${lang}/${file}${this.suffix}`)
         .pipe(map((translations) => ({ [file]: translations }))),
     );
-
+ 
     return forkJoin(requests).pipe(
+      catchError((err) => {
+        console.warn('error:', err);
+        return of([]);
+      }),
       map((results) =>
         results.reduce<TranslationObject>(
           (acc, curr) => ({ ...acc, ...curr }),
@@ -35,9 +43,11 @@ export class LazyTranslateLoader implements TranslateLoader {
     );
   }
 }
-
+ 
 export function createLazyTranslateLoader(
-  http: HttpClient,
+  httpBackend: HttpBackend,
 ): LazyTranslateLoader {
-  return new LazyTranslateLoader(http, '/i18n/', '.json');
+  return new LazyTranslateLoader(httpBackend, '/i18n/', '.json');
 }
+ 
+ 
